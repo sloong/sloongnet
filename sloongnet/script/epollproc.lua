18a5a07ca99dc472513cd98a8c75500d205cef07
Sansan Gao <gss@sloong.com>
2015-11-17 22:13:22 +0800

    modify scripr #2

--- sloongnet/script/epollproc.lua
+++ sloongnet/script/epollproc.lua
@@ -1,9 +1,21 @@
--- When mesaage is recved, the fm will call this function.
+-- When message is recved, the fm will call this function.
 OnRecvMessage = function( uinfo, request, result )
 	local msg = request:getdata("mesaage");
-    local res = string.split(msg,"|");
+    paserMessage(msg);
+	result:setdata("result","success");
+end
+
+
+-- send message to client
+SendMessage = function( uinfo,request,result )
+	result:setdata("type","send_message");
+	result:setdata("user_id","333");
+	result:setdata("message","hello");
+end
+
+local paserMessage = function( msg )
+	local res = string.split(msg,"|");
     for k,v in pairs(res) do
         print(v);
     end
-	result:setdata("result","success");
 end
