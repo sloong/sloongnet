cfd408a1276d1a5b28274e5c6fccfbf1aa574097
Wang Chuanbin <wcb@sloong.com>
2016-04-15 10:23:45 +0800

    try add auto version.sh

--- version.bat
+++
@@ -1,2 +0,0 @@
-git rev-list --all | wc -l
-pause
--- version.sh
+++ version.sh
@@ -1,1 +1,8 @@
-git rev-list --all | wc -l
+#!/bin/bash
+rm -f sloongnet/version.h
+git rev-list HEAD | sort > config.git-hash
+GIT_VERSION=`git rev-list --all | wc -l`
+
+cat version.h.template | sed "s/\$FULL_VERSION/$GIT_VERSION/g" > sloongnet/version.h
+ 
+echo "Generated version.h"
