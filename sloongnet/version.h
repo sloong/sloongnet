cefbc3b1e447bc6b0129b21df69fa6cc7904a439
Wang Chuanbin <wcb@sloong.com>
2016-04-21 16:18:09 +0800

    remove file

--- sloongnet/version.h
+++ sloongnet/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.6.2.160"
+#define VERSION_TEXT		"0.6.2.162"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2016 Sloong.com. All Rights Reserved "
 
