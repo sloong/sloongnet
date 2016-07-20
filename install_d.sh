b12c40a5971394e1cff4a2e56898bf830f2abc45
Wang Chuanbin <wcb@sloong.com>
2016-07-20 13:10:21 +0800

    脚本调整

--- install_d.sh
+++ install_d.sh
@@ -1,11 +1,11 @@
-./build.sh
+./build_d.sh
 rm -f /prod/sloong/sloongnet
 rm -rdf /prod/sloong/scripts
 mkdir /prod
 mkdir /prod/sloong
-cp -f output/sloongnet /prod/sloong/sloongnet
-cp -rdf output/scripts /prod/sloong/scripts
-cp -f output/default.conf /prod/sloong/default.conf
+cp -f output/debug/sloongnet /prod/sloong/sloongnet
+cp -rdf output/debug/scripts /prod/sloong/scripts
+cp -f output/debug/default.conf /prod/sloong/default.conf
 cd ../centos
 cp sloongnet /etc/init.d/sloongnet
 cp sloongnet.services /usr/lib/systemd/system/sloongnet.services
--- sloongnet/version.h
+++ sloongnet/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.6.6.194"
+#define VERSION_TEXT		"0.6.6.195"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2016 Sloong.com. All Rights Reserved "
 
