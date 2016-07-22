8d64f091c7cb041933f7297433882ba63370b74b
Wang Chuanbin <wcb@sloong.com>
2016-07-22 16:47:41 +0800

    version update

--- sloongnet/version.h
+++ sloongnet/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.7.0.202"
+#define VERSION_TEXT		"0.7.1.203"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2016 Sloong.com. All Rights Reserved "
 
