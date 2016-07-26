ef828dbdca070dfe43702822aa895ade34ba1bee
Wang Chuanbin <wcb@sloong.com>
2016-07-26 19:26:25 +0800

    测试脚本更新

--- sloongnet/scripts/main.lua
+++ sloongnet/scripts/main.lua
@@ -80,9 +80,9 @@ function main_Req.UploadWithTCPStart(u, req, res)
 		local path = '/tmp/sloong/'  .. filename;
 		fielList[md5] = path;
 	end
-	local res,msg = ReceiveFile(req['uuid'],17001,5600*1024,fielList,10,'/tmp/temp.tmp');
+	local suc_num,suc_list,errmsg = ReceiveFile(req['uuid'],17001,5600*1024,fielList,10,'/tmp/temp.tmp');
 	if res == 0 then
-		return -1,msg;
+		return -1,errmsg;
 	else
 		return 0;
 	end
