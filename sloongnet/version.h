9ef00b7f3f36e02c78ec620ceb1f4d12832e6aed
wcb <wcb@sloong.com>
2017-06-13 14:08:26 +0800

    update version

--- sloongnet/version.h
+++ sloongnet/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.7.7.237"
+#define VERSION_TEXT		"0.8.0.241"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2017 Sloong.com. All Rights Reserved "
 
