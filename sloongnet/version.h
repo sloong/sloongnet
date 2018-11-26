00c49dc2a7de9cfd512a4bdf164be5f1260778a0
wcb <wcb@sloong.com>
2018-11-26 19:45:12 +0800

    remove interface document. move to web site

--- sloongnet/version.h
+++ sloongnet/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.9.0.320"
+#define VERSION_TEXT		"0.9.0.321"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2018 Sloong.com. All Rights Reserved "
 
