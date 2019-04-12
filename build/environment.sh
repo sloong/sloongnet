e6191acc115bbd2df287506b3080e0a215eb4de7
wcb <wcb@sloong.com>
2019-04-12 19:24:09 +0800

    update environment.sh

--- build/environment.sh
+++ build/environment.sh
@@ -1,1 +1,1 @@
-sudo apt install libsqlite3-0 libsqlite3-dev libprotobuf10 libprotobuf-dev protobuf-compiler
+sudo apt install -y cmake libsqlite3-0 libsqlite3-dev libprotobuf10 libprotobuf-dev protobuf-compiler uuid-dev libglib2.0-dev
