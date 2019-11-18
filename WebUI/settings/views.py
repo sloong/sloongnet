eb0c17d39e442de0d7cd17efe2c16971d0bfd978
wcb <wcb@sloong.com>
2019-11-18 19:47:12 +0800

    WebUI update for config page. db file update. add unique index for configuation table.

--- WebUI/settings/views.py
+++ WebUI/settings/views.py
@@ -2,7 +2,7 @@
 @Author: WCB
 @Date: 2019-11-14 14:05:25
 @LastEditors: WCB
-@LastEditTime: 2019-11-18 16:56:10
+@LastEditTime: 2019-11-18 19:42:24
 @Description: file conten
 '''
 from django.shortcuts import render
@@ -44,22 +44,22 @@ def config_show(request,uuid):
     3. 是否必填
     4. 规则描述。int为tuple(min,max)。string为正则表达式。enum为enum列表。None为不启用
     '''
-    config_list['ModuleType'] =  tuple( 'tuple', 1, True, tuple(protocol.ModuleType.items()[1:]))
-    config_list['ListenPort'] = tuple( 'int', 8002, True, tuple( 1000,65535 ))
-    config_list['EnableSSL'] =  tuple( 'bool', False, True, None )
-    config_list['CertFilePath'] = tuple( 'str', '', False, None) 
-    config_list['KeyFilePath'] = tuple( 'str','', False, None) 
-    config_list['CertPasswd'] = tuple( 'str','', False, None) 
-    config_list['ConnectTime'] = tuple( 'int',5, True, tuple(0, 20))
-    config_list['ReceiveTime'] = tuple( 'int',5, True, tuple(0, 20))
-    config_list['LogPath'] = tuple( 'str','', True, None)
-    config_list['LogLevel']= tuple( 'tuple',protocol.LogLevel.Info, True, tuple(protocol.LogLevel.items())) 
-    config_list['DebugMode']= tuple( 'bool',False, True, None )
-    config_list['MQThreadQuantity']=  tuple( 'int',1, True, tuple(1, 20))
-    config_list['ProcessThreadQuantity']=  tuple( 'int',1, True, tuple(1, 20))
-    config_list['PrioritySize']=  tuple( 'int',3, True, tuple(0, 20))
-
+    config_list['ModuleType'] =  ( 'tuple', 1, True, (protocol.ModuleType.items()[2:]))
+    config_list['ListenPort'] = ( 'int', 8002, True, ( 1000,65535 ))
+    config_list['EnableSSL'] =  ( 'bool', False, True, None )
+    config_list['CertFilePath'] = ( 'str', '', False, None) 
+    config_list['KeyFilePath'] = ( 'str','', False, None) 
+    config_list['CertPasswd'] = ( 'str','', False, None) 
+    config_list['ConnectTime'] = ( 'int',5, True, (0, 20))
+    config_list['ReceiveTime'] = ( 'int',5, True, (0, 20))
+    config_list['LogPath'] = ( 'str','/data/log/', True, None)
+    config_list['LogLevel']= ( 'tuple',protocol.LogLevel.Info, True, (protocol.LogLevel.items())) 
+    config_list['DebugMode']= ( 'bool',False, True, None )
+    config_list['MQThreadQuantity']=  ( 'int',1, True, (1, 20))
+    config_list['ProcessThreadQuantity']=  ( 'int',1, True, (1, 20))
+    config_list['PrioritySize']=  ( 'int',3, True, (0, 20))
     c['ConfigList'] = config_list
+    c['Message']=''
     return render(request, 'config.html', c)
 
 def config_response(request,uuid):
