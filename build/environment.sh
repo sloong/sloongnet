202fa8bd3b03d077a4a121a733dfe962d9e8637f
wcb <wcb@sloong.com>
2020-03-12 11:52:45 +0800

    convert to unix format.

--- build/environment.sh
+++ build/environment.sh
@@ -1,12 +1,1 @@
-### 
-# @Author: WCB
- # @Date: 2019-11-05 08:59:19
- # @LastEditors: WCB
- # @LastEditTime: 2019-12-10 15:04:57
- # @Description: file content
- ###
-sudo apt-get update
-# install run time library
-sudo apt install -y libsqlite3-0 libprotobuf10 libuuid1 libglib2.0-0 libssl1.1 libboost-serialization1.65.1 liblua5.3-0 libjsoncpp1 zip
-# install develop library
-sudo apt install -y cmake gcc g++ gdb libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libglib2.0-dev libssl-dev libboost-dev libboost-serialization-dev liblua5.3-dev libjsoncpp-dev
+#!/bin/bash### # @Author: WCB # @Date: 2019-11-05 08:59:19 # @LastEditors: WCB # @LastEditTime: 2019-12-10 15:04:57 # @Description: file content ###sudo apt-get update# install run time librarysudo apt install -y libsqlite3-0 libprotobuf10 libuuid1 libglib2.0-0 libssl1.1 libboost-serialization1.65.1 liblua5.3-0 libjsoncpp1 zip# install develop librarysudo apt install -y cmake gcc g++ gdb libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libglib2.0-dev libssl-dev libboost-dev libboost-serialization-dev liblua5.3-dev libjsoncpp-dev
