923f5fb6e70c55bcecb500b8b51f626fb82f473c
wcb <wcb@sloong.com>
2020-06-04 20:14:08 +0800

    Add set package size function.

--- sloongnet/modules/core/DataTransPackage.h
+++ sloongnet/modules/core/DataTransPackage.h
@@ -119,9 +119,10 @@ namespace Sloong
         SOCKET m_nSocket = INVALID_SOCKET;
 
     public:
-        static inline void InitializeLog(CLog *log) { g_pLog = log; }
         static CLog *g_pLog;
+        static inline void InitializeLog(CLog *log) { g_pLog = log; }
         static int g_max_package_size = 5* 1024 *1024;
+        static inline void SetPackageSizeLimit( int size_of_byte ){  g_max_package_size = size_of_byte; }
     };
     typedef unique_ptr<CDataTransPackage> UniqueTransPackage;
 } // namespace Sloong
