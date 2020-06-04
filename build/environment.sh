1f4ec41e7e09546c53446f3dca45f292d7cf04ca
wcb <wcb@sloong.com>
2020-06-04 12:33:15 +0800

    Add env reqire for mysql.

--- build/environment.sh
+++ build/environment.sh
@@ -8,6 +8,10 @@
 #!/bin/bash
 sudo apt-get update
 # install run time library
-sudo apt install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1 libluajit-5.1 libjsoncpp1 zip
+sudo apt install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1  libjsoncpp1 zip
 # install develop library
-sudo apt install -y cmake gcc g++ gdb libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libluajit-5.1-dev libjsoncpp-dev
+sudo apt install -y cmake gcc g++ gdb libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev
+# for datacenter
+sudo apt install -y mariaclient mariaclient-dev
+# for lua middle layer
+sudo apt install -y libluajit-5.1 libluajit-5.1-dev
