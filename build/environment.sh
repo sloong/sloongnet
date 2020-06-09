1dab908378063d653fc4d3a601e93755f6b3a0fd
wcb <wcb@sloong.com>
2020-06-09 14:22:56 +0800

    The environment add mariadb.

--- build/environment.sh
+++ build/environment.sh
@@ -8,10 +8,10 @@
 #!/bin/bash
 apt-get update
 # install run time library
-apt install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1  libjsoncpp1 zip
+apt install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1  libjsoncpp1 zip 
 # install develop library
 apt install -y cmake gcc g++ gdb libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev
 # for datacenter
-apt install -y mariaclient mariaclient-dev
+apt install -y mariadb-client libmariadbclient-dev
 # for lua middle layer
 apt install -y libluajit-5.1 libluajit-5.1-dev
