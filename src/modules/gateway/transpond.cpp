74d47e3300a43af2f654f7c032746beba01ad7d0
wcb <wcb@sloong.com>
2020-07-29 19:46:47 +0800

    Fix gateway problem.

--- src/modules/gateway/transpond.cpp
+++ src/modules/gateway/transpond.cpp
@@ -1,7 +1,7 @@
 /*** 
  * @Author: Chuanbin Wang
  * @Date: 2020-04-28 14:43:16
- * @LastEditTime: 2020-07-27 15:00:48
+ * @LastEditTime: 2020-07-29 17:23:24
  * @LastEditors: Chuanbin Wang
  * @FilePath: /engine/src/modules/gateway/transpond.cpp
  * @Copyright 2015-2020 Sloong.com. All Rights Reserved
@@ -33,11 +33,11 @@ PackageResult GatewayTranspond::ResponsePackageProcesser( UniquePackage info, Da
 PackageResult Sloong::GatewayTranspond::MessageToProcesser(DataPackage *pack)
 {
 	m_pLog->Debug("Receive new request package.");
-	auto target = SloongNetGateway::Instance->GetPorcessConnect(pack->function());
-	if( target == INVALID_SOCKET )
+	auto target = SloongNetGateway::Instance->GetPorcessConnection(pack->function());
+	if( target == 0 )
 	{
-		m_pLog->Debug(Helper::Format("No find process service for function[%d]. package [%d][%llu]", pack->function(),  pack->reserved().sessionid(), pack->id() ));
-		return PackageResult::Make_Error(Helper::Format("No find process service for function[%d]. package [%d][%llu]", pack->function(),  pack->reserved().sessionid(), pack->id() ));
+		m_pLog->Debug(Helper::Format("No find process service for function[%d]. package [%d][%lld]", pack->function(),  pack->reserved().sessionid(), pack->id() ));
+		return PackageResult::Make_Error(Helper::Format("No find process service for function[%d]. package [%d][%lld]", pack->function(),  pack->reserved().sessionid(), pack->id() ));
 	}
 
 	auto response = Package::MakeResponse(pack);
@@ -48,14 +48,15 @@ PackageResult Sloong::GatewayTranspond::MessageToProcesser(DataPackage *pack)
 	response->mutable_reserved()->add_clocks(GetClock());
 
 	auto trans_pack = Package::MakeResponse(pack);
+	trans_pack->set_status(DataPackage_StatusType::DataPackage_StatusType_Request);
 	
 	auto id = snowflake::Instance->nextid();
 	trans_pack->set_id(id);
 	trans_pack->mutable_reserved()->set_sessionid(target);
 	
-	m_pLog->Debug(Helper::Format("Trans package [%lld][%llu] -> [%d][%llu]", pack->reserved().sessionid(), pack->id(), trans_pack->reserved().sessionid(), trans_pack->id()));
+	m_pLog->Debug(Helper::Format("Trans package [%lld][%lld] -> [%d][%lld]", pack->reserved().sessionid(), pack->id(), trans_pack->reserved().sessionid(), trans_pack->id()));
 
-	SloongNetGateway::Instance->m_mapSerialToRequest[trans_pack->reserved().sessionid()] = move(response);
+	SloongNetGateway::Instance->m_mapSerialToRequest[trans_pack->id()] = move(response);
 	return PackageResult::Make_OK(move(trans_pack));
 }
 
