4d53dcbad262845267c80539919ab40a32077944
wcb <wcb@sloong.com>
2020-12-30 19:44:17 +0800

    fix(build):add pie param. it's an error when build with release.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.12.0.803"
+#define VERSION_TEXT		"0.12.0.804"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2020 Sloong.com. All Rights Reserved "
 
