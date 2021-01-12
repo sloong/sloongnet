4d7da81154d18f6655c99af5686ca42cca15bb25
wcb <wcb@sloong.com>
2021-01-12 14:52:43 +0800

    update version to v0.12

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.11.1.817"
+#define VERSION_TEXT		"0.12.0.818"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2021 Sloong.com. All Rights Reserved "
 
