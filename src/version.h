b351695f4a81ac54e72a0b2bd3a1780be5ebe3b7
wcb <wcb@sloong.com>
2021-01-14 18:48:26 +0800

    Rename the environment variable name for db file path.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.12.0.825"
+#define VERSION_TEXT		"0.12.0.828"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2021 Sloong.com. All Rights Reserved "
 
