56f7d3430d478a700c07dccc0962bd85d4fd4f8b
wcb <wcb@sloong.com>
2021-03-26 14:54:19 +0800

    doc(lua): add engine interface for lua details.

--- src/version.h
+++ src/version.h
@@ -1,6 +1,6 @@
 #ifndef VERSION_H
 
-#define VERSION_TEXT		"0.12.0.859"
+#define VERSION_TEXT		"0.12.0.860"
 #define PRODUCT_TEXT		"Sloong Network Engine"
 #define COPYRIGHT_TEXT		"Copyright 2015-2021 Sloong.com. All Rights Reserved "
 
