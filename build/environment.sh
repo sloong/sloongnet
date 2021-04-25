8e3db2f93ad0482b12dee58bdb5bb215e0046e15
wcb <wcb@sloong.com>
2021-04-25 16:26:07 +0800

    build: change build script.

--- build/environment.sh
+++ build/environment.sh
@@ -2,12 +2,12 @@
  # @Author: WCB
  # @Date: 2020-04-20 12:26:04
  # @LastEditors: Chuanbin Wang
- # @LastEditTime: 2020-08-17 10:42:47
+ # @LastEditTime: 2021-04-25 16:25:15
  # @Description: file content
  ###
 #!/bin/bash
 apt-get update
 # install run time library
-apt install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1  libjsoncpp1 mariadb-client libluajit-5.1-2 graphicsmagick
+apt-get install -y libsqlite3-0 libprotobuf17 libuuid1 libssl1.1  libjsoncpp1 mariadb-client libluajit-5.1-2 graphicsmagick
 # install develop library
-apt install -y cmake clang llvm libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev libmariadbclient-dev libluajit-5.1-dev
+apt-get install -y cmake clang llvm libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev libmariadbclient-dev libluajit-5.1-dev
