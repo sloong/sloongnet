d4b2f38eecbf29c7ffeef61eb307692f4c68f2f8
wcb <wcb@sloong.com>
2021-08-24 15:47:22 +0800

    build: fix for debian 10 /11.

--- build/environment.sh
+++ build/environment.sh
@@ -3,7 +3,7 @@
  # @Author: WCB
  # @Date: 2020-04-20 12:26:04
  # @LastEditors: Chuanbin Wang
- # @LastEditTime: 2021-04-25 16:54:17
+ # @LastEditTime: 2021-08-24 15:44:11
  # @Description: file content
  ###
 
@@ -12,6 +12,10 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 # install run time library
-apt-get install -y libsqlite3-0 libprotobuf23 libuuid1 libssl1.1  libjsoncpp24 mariadb-client liblua5.3-0 imagemagick
+# for debian 11/ubuntu 21.04
+apt install -y libprotobuf23 libjsoncpp24
+# for debian 10/ubuntu 20.04
+apt install -y libprotobuf17 libjsoncpp1
+apt-get install -y libsqlite3-0 libuuid1 libssl1.1 mariadb-client liblua5.3-0 imagemagick
 # install develop library
 apt-get install -y cmake clang llvm libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev libmariadb-dev liblua5.3-dev
