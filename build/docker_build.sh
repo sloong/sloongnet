a6ab7963eff5c04df6b16d1fb567cbe3b17975db
wcb <wcb@sloong.com>
2021-08-26 15:02:22 +0800

    docker: before build docker image, call script with -c to clean cache.

--- build/docker_build.sh
+++ build/docker_build.sh
@@ -2,8 +2,8 @@
 ### 
 # @Author: WCB
  # @Date: 2019-12-11 14:28:05
- # @LastEditors  : WCB
- # @LastEditTime : 2019-12-26 11:22:04
+ # @LastEditors: Chuanbin Wang
+ # @LastEditTime: 2021-08-26 13:55:08
  # @Description: file content
  ###
 pwd
@@ -14,5 +14,6 @@ cd $SCRIPTFOLDER/../
 
 VERSION_STR=$(cat version)
 
+./build/build.sh -c
 docker build -t sloongnet:$VERSION_STR -f $SCRIPTFOLDER/../Dockerfile $SCRIPTFOLDER/../
 docker tag sloongnet:$VERSION_STR sloongnet:latest
