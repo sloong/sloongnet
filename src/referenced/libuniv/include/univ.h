dd08632d4f1fe002712beebed29a02d6ed46d5fb
wcb <wcb@sloong.com>
2021-09-15 10:51:44 +0800

    fix(#37): this problem was caused by the univ log. and now it's replace by spdlog.

--- src/referenced/libuniv/include/univ.h
+++ src/referenced/libuniv/include/univ.h
@@ -1,9 +1,9 @@
 /*** 
  * @Author: Chuanbin Wang - wcb@sloong.com
  * @Date: 2021-03-25 15:52:09
- * @LastEditTime: 2021-08-27 10:32:37
+ * @LastEditTime: 2021-09-15 10:49:33
  * @LastEditors: Chuanbin Wang
- * @FilePath: /libuniv/src/univ.h
+ * @FilePath: /engine/src/referenced/libuniv/include/univ.h
  * Copyright 2015-2020 Sloong.com. All Rights Reserved
  * @Description: 
  */
@@ -22,7 +22,6 @@
 #include "helper.hpp"
 #include "defines.h"
 #include "univ.h"
-#include "log.h"
 #include "threadpool.h"
 #include "taskpool.h"
 #include "hash.h"
