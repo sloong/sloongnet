5b130d904e6ffaff4f3efda89997767d69d4884a
wcb <wcb@sloong.com>
2021-10-20 10:52:13 +0800

    docker: try optimize the image size.

--- build/Run.Dockerfile
+++ build/Run.Dockerfile
@@ -1,15 +1,26 @@
 FROM debian:11-slim
 
 # chagne to ustc source
-RUN sed -i "s/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
-RUN sed -i "s/security.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list
-RUN apt update && apt install -y apt-transport-https ca-certificates
-RUN sed -i "s/http/https/g" /etc/apt/sources.list
+RUN apt update && apt install -y \
+    apt-transport-https ca-certificates \
+ && apt clean \
+ && apt autoremove --yes \
+ && rm -rf /var/lib/apt/lists/*
+
+
+RUN sed -i "s/deb.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
+ && sed -i "s/security.debian.org/mirrors.tuna.tsinghua.edu.cn/g" /etc/apt/sources.list \
+ && sed -i "s/http/https/g" /etc/apt/sources.list \
+ && apt update 
 
 # copy file to docker
 COPY . /tmp/
 WORKDIR /tmp
 
 # install build packages
-RUN /tmp/build/environment.sh --run
-RUN rm -rdf /tmp && mkdir /tmp
+RUN /tmp/build/environment.sh --run \
+ && apt clean \
+ && apt autoremove --yes \
+ && rm -rf /var/lib/apt/lists/* \
+ && rm -rdf /tmp \
+ && mkdir /tmp
--- build/environment.sh
+++ build/environment.sh
@@ -3,7 +3,7 @@
  # @Author: WCB
  # @Date: 2020-04-20 12:26:04
  # @LastEditors: Chuanbin Wang
- # @LastEditTime: 2021-09-15 11:37:28
+ # @LastEditTime: 2021-10-20 10:41:35
  # @Description: file content
  ###
 
@@ -11,23 +11,23 @@
 export DEBIAN_FRONTEND=noninteractive
 
 update(){
-    DEBIAN_FRONTEND=noninteractive apt-get update
+    DEBIAN_FRONTEND=noninteractive apt update
     DEBIAN_FRONTEND=noninteractive apt install -y tzdata
 }
 
 install_run_env(){
     # install run time library
     # for debian 11/ubuntu 21.04
-    apt-get install -y libprotobuf23 libjsoncpp24 libfmt7
+    apt install -y libprotobuf23 libjsoncpp24 libfmt7
     # for debian 10/ubuntu 20.04
     # apt install -y libprotobuf17 libjsoncpp1 libfmt6
-    apt-get install -y libsqlite3-0 libuuid1 libssl1.1 mariadb-client liblua5.3-0 imagemagick
+    apt install -y libsqlite3-0 libuuid1 libssl1.1 mariadb-client liblua5.3-0 imagemagick
 }
 
 
 install_build_env(){
     # install develop library
-    apt-get install -y cmake clang llvm libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev libmariadb-dev liblua5.3-dev libfmt-dev libspdlog-dev
+    apt install -y cmake clang llvm libsqlite3-dev libprotobuf-dev protobuf-compiler uuid-dev libssl-dev libjsoncpp-dev libmariadb-dev liblua5.3-dev libfmt-dev libspdlog-dev
 }
 
 install_all(){
